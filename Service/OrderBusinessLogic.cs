8674c896854c82383dfa4b8a8e710eea771aa5e9
marcel <lis.marcys@gmail.com>
2025-03-16 21:52:17 +0100

    Beinnig of business logic implementation
    - Added `OrderBusinessLogic` class to handle specific logic cases
    - Removed `CustomerData` class from DTO as for now it's not going to be developed

---
+++ Service/OrderBusinessLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessor.Service
+{
+    class OrderBusinessLogic
+    {
+        public OrderBusinessLogic() { }
+
+        #region Public Methods
+        public void IsOrderEligibleForWarehouseProcessing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkOrderAsShippedAfterDelay()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
