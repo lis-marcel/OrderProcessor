e93b4327bc88f60e62cdfd7596d97fdce3739f6c
marcel <lis.marcys@gmail.com>
2025-04-19 21:00:47 +0200

    Initalized web API

---
+++ src/OrderProcessor.Web.API/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderProcessor.Web.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    
+}
